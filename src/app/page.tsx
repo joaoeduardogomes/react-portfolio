b719d466506798390ee1d11b976501f9e2cbd9c0
João Gomes <joaoeduardo.gomes21@gmail.com>
2026-02-26 17:16:20 -0300

    change background to container div
    + scss variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from "./page.module.scss";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
+      <main className={`${styles.main} ${styles.box}`}>
       
         
       </main>
