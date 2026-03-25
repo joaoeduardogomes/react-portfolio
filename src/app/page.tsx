2bc29bb688156b4d7f4cfdadfd7f51b43e1299ff
João Gomes <joaoeduardo.gomes21@gmail.com>
2026-03-25 20:46:13 -0300

    [change] about page + coffee bar css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,41 +2,48 @@ import { Metadata } from "next";
 import styles from "./page.module.scss";
 
 export const metadata: Metadata = {
-    title: "João Eduardo",
-    description: "About João Eduardo, web developer focused on clean and maintainable code."
+  title: "João Eduardo",
+  description: "About João Eduardo, web developer focused on clean and maintainable code."
 }
 
 export default function About() {
   return (
     <>
-      <img src="/images/avatar-nobg.png" alt="avatar" />
-      <div className={styles['scoffee-bar']}>
-        <p>Coffee</p>
-        <p>80/100</p>
-        <progress value="80" max="100"></progress>
-      </div>
+      <section className={styles.info}>
+        <div className={styles.user}>
+          <img src="/images/avatar-nobg.png" alt="avatar" />
+          <div className={styles['coffee-bar']}>
+            <p>
+              <span>Coffee</span>
+              <span>80/100</span>
+            </p>
+            <progress value="80" max="100"></progress>
+          </div>
+        </div>
+        <table className={styles['info-table']}>
+          <caption className="visually-hidden">Basic Information</caption>
+          <tbody>
+            <tr>
+              <th scope="row">Name</th>
+              <td>João Eduardo</td>
+            </tr>
+            <tr>
+              <th scope="row">Age</th>
+              <td>32</td>
+            </tr>
+            <tr>
+              <th scope="row">Role</th>
+              <td>Web Developer</td>
+            </tr>
+            <tr>
+              <th scope="row">Location</th>
+              <td>Brazil</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
 
-      <table className={styles.infoTable}>
-        <caption className="visually-hidden">Basic Information</caption>
-        <tbody>
-          <tr>
-            <th scope="row">Name</th>
-            <td>João Eduardo</td>
-          </tr>
-          <tr>
-            <th scope="row">Age</th>
-            <td>32</td>
-          </tr>
-          <tr>
-            <th scope="row">Role</th>
-            <td>Web Developer</td>
-          </tr>
-          <tr>
-            <th scope="row">Location</th>
-            <td>Brazil</td>
-          </tr>
-        </tbody>
-      </table>
+
 
       <h1>About Me</h1>
       <p>
