24010773560c71789dc133c33c6555b46c0b2cf0
João Gomes <86986385+joaoeduardogomes@users.noreply.github.com>
2026-04-12 15:55:11 -0300

    add hobbies page css

--- src/app/hobbies/page.tsx
+++ src/app/hobbies/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
 import Image from "next/image";
+import styles from "./page.module.scss";
 
 export const metadata: Metadata = {
     title: "Hobbies",
@@ -12,14 +13,20 @@ export default function Hobbies() {
     return (
         <>
             <h1 className="visually-hidden">Hobbies</h1>
-            <Image src="/images/treasure.jpg" width={width} height={height} alt="treasure image" />
+            <Image
+                src="/images/treasure.jpg"
+                width={width}
+                height={height}
+                alt="treasure image"
+                className={styles.image}
+            />
 
-            <ul>
-                <li>Videogames & RPGs</li>
-                <li>Reading (tech & fiction)</li>
-                <li>Anime & manga</li>
-                <li>Japanese language & culture</li>
-                <li>Writing & translation</li>
+            <ul className={styles.list}>
+                <li>🎮 Videogames & RPGs</li>
+                <li>📚 Reading (tech & fiction)</li>
+                <li>🎴 Anime & manga</li>
+                <li>🈶 Japanese language & culture</li>
+                <li>✍️ Writing & translation</li>
             </ul>
         </>
     )
