6a595478564fcbd6734d11a2b6bebe69bee5268d
João Gomes <86986385+joaoeduardogomes@users.noreply.github.com>
2026-04-13 09:26:08 -0300

    404 page css

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
+import styles from "./not-found.module.scss";
 
 export default function NotFound() {
     return (
-        <div>
+        <div className={styles.error}>
             <h1>404</h1>
             <p>This page does not exist.</p>
 
-            <Link href="/">Return home</Link>
+            <Link href="/" className={styles['return-btn']}>Return home</Link>
         </div>
     );
 }
