bd8c0e849bc26cfeb5694c1d0c0fd1a5dbcb2abf
João Gomes <86986385+joaoeduardogomes@users.noreply.github.com>
2026-04-13 10:05:42 -0300

    [add] faq page css

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
 import { faqData } from "@/data/faq";
+import styles from "./page.module.scss";
 
 export const metadata: Metadata = {
     title: "FAQ",
@@ -11,11 +12,11 @@ export default function FAQ() {
         <>
             <h1 className="visually-hidden">FAQ</h1>
             
-            <dl>
+            <dl className={styles['question-block']}>
                 {faqData.map((item) => (
                     <div key={item.id}>
-                        <dt>{item.question}</dt>
-                        <dd>{item.answer}</dd>
+                        <dt className={styles.question}>{item.question}</dt>
+                        <dd className={styles.answer}>{item.answer}</dd>
                     </div>
                 ))}
             </dl>
