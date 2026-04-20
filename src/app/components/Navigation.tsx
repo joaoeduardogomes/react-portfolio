17d67ecbcb504abdf7693474730a5bea4048b91c
João Gomes <86986385+joaoeduardogomes@users.noreply.github.com>
2026-04-20 19:11:12 -0300

    [navbar] responsive menu

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -3,6 +3,8 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import styles from "./Navigation.module.scss";
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
 
 export default function Navigation() {
     const pathname = usePathname();
@@ -17,20 +19,34 @@ export default function Navigation() {
         { href: "/faq", label: "FAQ" }
     ];
 
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-        <nav className={styles.navbar}>
-            <ul>
-                {links.map(link => (
-                    <li key={link.href}>
-                        <Link
-                            href={link.href}
-                            className={pathname === link.href ? styles.active : ""}
-                        >
-                            {link.label}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-        </nav>
+        <>
+            <nav className={styles.navbar}>
+                <button
+                    type="button"
+                    onClick={() => setIsOpen(!isOpen)}
+                    className={styles["nav-btn"]}
+                    aria-label={isOpen ? "Fechar menu" : "Abrir menu"}
+                    aria-expanded={isOpen}
+                >
+                    {isOpen ? <X size={28} /> : <Menu size={28} />}
+                </button>
+
+                <ul className={isOpen ? styles.open : ""}>
+                    {links.map(link => (
+                        <li key={link.href}>
+                            <Link
+                                href={link.href}
+                                className={pathname === link.href ? styles.active : ""}
+                            >
+                                {link.label}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </>
     );
 }
